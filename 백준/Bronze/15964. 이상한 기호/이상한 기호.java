8d5d15a3af878e754eaa7f7aa2b7dcf8a4e7e8ad
bin <121717476+Hongjunbin@users.noreply.github.com>
2024-10-24 23:07:52 +0900

    [Bronze V] Title: 이상한 기호, Time: 180 ms, Memory: 17744 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        calculate(a, b);
+        long a = scanner.nextInt();
+        long b = scanner.nextInt();
+        System.out.println(calculate(a, b));
     }
 
-    public static void calculate(int a, int b) {
-        System.out.print((a + b) * (a - b));
+    public static long calculate(long a, long b) {
+        return (a + b) * (a - b);
     }
 }
