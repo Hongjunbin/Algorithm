fc352a297358dbf59ca0609f8427d9915cbfbc9f
bin <121717476+Hongjunbin@users.noreply.github.com>
2024-11-06 19:31:16 +0900

    [Bronze II] Title: 분산처리, Time: 208 ms, Memory: 18268 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.java
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -12,26 +12,31 @@ public class Main {
         for (int i = 0; i < t; i++) {
             int a = scanner.nextInt();
             int b = scanner.nextInt();
-            int lastDigit = a % 10;
-
-            List<Integer> cycle = new ArrayList<>();
-            int current = lastDigit;
-
-            while (!cycle.contains(current)) {
-                cycle.add(current);
-                current = (current * lastDigit) % 10;
-            }
-
-            int index = (b % cycle.size() == 0) ? cycle.size() - 1 : (b % cycle.size()) - 1;
-            int computerNumber = cycle.get(index);
-
-            if (computerNumber == 0) {
-                computerNumber = 10;
-            }
+            int computerNumber = getComputerNumber(a, b);
 
             result.append(computerNumber).append("\n");
         }
 
         System.out.print(result);
+    }
+
+    private static int getComputerNumber(int a, int b) {
+        int lastDigit = a % 10;
+
+        List<Integer> cycle = new ArrayList<>();
+        int current = lastDigit;
+
+        while (!cycle.contains(current)) {
+            cycle.add(current);
+            current = (current * lastDigit) % 10;
+        }
+
+        int index = (b % cycle.size() == 0) ? cycle.size() - 1 : (b % cycle.size()) - 1;
+        int computerNumber = cycle.get(index);
+
+        if (computerNumber == 0) {
+            computerNumber = 10;
+        }
+        return computerNumber;
     }
 }
