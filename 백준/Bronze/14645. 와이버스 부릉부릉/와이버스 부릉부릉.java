190683b7e3618f06ba5a4a274041d634e651b7db
bin <121717476+Hongjunbin@users.noreply.github.com>
2024-11-24 19:21:01 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 168 ms, Memory: 17504 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); // 정거장 총 갯수
+        int k = scanner.nextInt(); // 출발역 탑승 사람 수
+        for(int i = 0; i < n; i++) {
+            int a = scanner.nextInt(); // 탑승 인원 A
+            int b = scanner.nextInt(); // 하차 인원 B
+        }
+        System.out.println("비와이");
+    }
+}
